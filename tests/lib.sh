5a64397e9a98fb3135bc6a05c448d67317586f39
Hannes Hörl <hannes.hoerl@snowreporter.com>
2017-08-28 01:05:37 +0100

    Untabify

--- tests/lib.sh
+++ tests/lib.sh
@@ -6,24 +6,24 @@ set -o pipefail
 . "$( dirname "$0" )/../assets/lib.sh"
 
 run_tests() {
-	local input='
+  local input='
 {
-	"source": {
-		"watchfile": "version=4\nhttps://golang.org/dl/ https://storage.googleapis.com/golang/go(1\\.8\\.3).linux-amd64.tar.gz"
-	},
-	"version": { "ref" : "0.0.1" }
+  "source": {
+    "watchfile": "version=4\nhttps://golang.org/dl/ https://storage.googleapis.com/golang/go(1\\.8\\.3).linux-amd64.tar.gz"
+  },
+  "version": { "ref" : "0.0.1" }
 }
 '
 
-	local version="$( echo "$input" | uscan_report | get_version )"
-	local expected_version="1.8.3"
+  local version="$( echo "$input" | uscan_report | get_version )"
+  local expected_version="1.8.3"
 
-	[ "$version" = "$expected_version" ] || {
-		echo "report not as expected"
-		exit 1
-	}
+  [ "$version" = "$expected_version" ] || {
+    echo "report not as expected"
+    exit 1
+  }
 }
 
 run_tests && {
-	echo 'all tests passed'
+  echo 'all tests passed'
 }
